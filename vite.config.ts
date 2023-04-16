db0a07966e0c596df7717708b879518fcae7f368
Robert Liebowitz <rliebz@gmail.com>
2023-04-16 14:30:25 -0400

    Silence unhelpful warning

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/word-hex/",
   plugins: [react()],
+  build: {
+    // the dictionary adds around 1000kb, so we'll add 1000 to the default
+    chunkSizeWarningLimit: 1500,
+  },
   test: {
     environment: "happy-dom",
   },
